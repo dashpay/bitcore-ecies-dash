2eaf64c39a447a1fa16d49b63868bad685c4ea96
Cofresi <cofresi@dash.org>
2018-09-23 12:04:08 -0400

    update deps

--- test/ecies.js
+++ test/ecies.js
@@ -3,7 +3,7 @@
 var ECIES = require('../');
 
 var should = require('chai').should();
-var bitcore = require('bitcore-lib-dash');
+var bitcore = require('@dashevo/dashcore-lib');
 var PrivateKey = bitcore.PrivateKey;
 
 
